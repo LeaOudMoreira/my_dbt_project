38f404b0013a04598ce2f952822e4ec4bdcd181c
Léa OUDART <froudlea@carlsberggroup.com>
2026-03-29 17:18:11 +0200

    fix error
    Signed-off-by: Léa OUDART <froudlea@carlsberggroup.com>

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,10 +1,10 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id_x,
- order_id,
- product_id,
- seller_id,
+select CONCAT(order_id, '_', product_id) AS order_item_id_y,
+order_id as order_id_y,
+ product_id as product_id_y,
+ seller_id as seller_id_y,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price as unit_price_x,
- shipping_cost,
- quantity as item_quantity_x,
- (price * quantity) + shipping_cost as total_order_item_amount_x
+ price as unit_price_y,
+ shipping_cost as shipping_cost_y,
+ quantity as item_quantity_y,
+ (price * quantity) + shipping_cost as total_order_item_amount_y
 from {{ source('sales_database', 'order_item') }}
